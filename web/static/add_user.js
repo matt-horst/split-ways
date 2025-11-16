d3bec7bd0d3403885f4ecda726f0b61540545126
Matt Horst <mattahorst@gmail.com>
2025-11-16 14:45:08 -0700

    adds js for add user

---
+++ web/static/add_user.js
@@ -0,0 +1,36 @@
+import { showError, showResult, hide } from "./status.js"
+
+const inputUsername = document.getElementById("input-username");
+const form = document.getElementById("form");
+const status = document.getElementById("status")
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    hide(status)
+
+    const username = inputUsername.value;
+
+    try {
+        const resp = await fetch(
+            "/api/groups/" + groupID + "/users",
+            {
+                method: "POST",
+                header: {"Content-Type": "application/json"},
+                body: JSON.stringify({"username": username}),
+                credentials: "same-origin"
+            }
+        );
+
+        if (!resp.ok) {
+            const msg = await resp.text();
+            showError(status, msg);
+            console.log(`${resp.status}: ${msg}`);
+        } else {
+            window.location.href = "/dashboard";
+        }
+    } catch (e) {
+        console.log(e)
+    }
+});
+
