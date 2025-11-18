fb307a9c02210fb47378eef9434520ab797d46c8
Matt Horst <mattahorst@gmail.com>
2025-11-18 08:04:37 -0700

    redirects to the group page from add user on success

--- web/static/add_user.js
+++ web/static/add_user.js
@@ -27,7 +27,7 @@ form.addEventListener("submit", async (event) => {
             showError(status, msg);
             console.log(`${resp.status}: ${msg}`);
         } else {
-            window.location.href = "/dashboard";
+            window.location.href = `/groups/${groupID}`;
         }
     } catch (e) {
         console.log(e)
