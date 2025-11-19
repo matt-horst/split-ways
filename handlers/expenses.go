face4efcfb2f80e742bff107aaffc2abb1d0969b
Matt Horst <mattahorst@gmail.com>
2025-11-18 20:39:29 -0700

    adds api end point to update an expense

--- handlers/expenses.go
+++ handlers/expenses.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -137,4 +138,131 @@ func (cfg *Config) HandlerCreateExpense(w http.ResponseWriter, r *http.Request) 
 		log.Printf("Couldn't send response body: %v\n", err)
 		return
 	}
+}
+
+func (cfg *Config) HandlerEditExpense(w http.ResponseWriter, r *http.Request) {
+	user, ok := r.Context().Value(userContextKey).(database.User)
+	if !ok {
+		log.Printf("Attempt to edit expense for unauthorized user\n")
+		http.Error(w, "User unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	id, err := uuid.Parse(r.URL.Query().Get("id"))
+	if err != nil {
+		log.Printf("Couldn't parse expense id: %v\n", err)
+		http.Error(w, "Couldn't find expense", http.StatusBadRequest)
+		return
+	}
+
+	tx, err := cfg.Db.GetTransaction(r.Context(), id)
+	if err != nil {
+		log.Printf("Couldn't get transaction: %v\n", err)
+		http.Error(w, "Couldn't find transaction", http.StatusBadRequest)
+		return
+	}
+
+	if !tx.CreatedBy.Valid || tx.CreatedBy.UUID != user.ID {
+		log.Printf("Attempt to edit expense for unauthorized user\n")
+		http.Error(w, "Can't edit other users' expenses", http.StatusForbidden)
+		return
+	}
+
+	expense, err := cfg.Db.GetExpenseByTransaction(r.Context(), tx.ID)
+	if err != nil {
+		log.Printf("Couldn't find expense by transaction: %v\n", err)
+		http.Error(w, "Couldn't find expense", http.StatusBadRequest)
+		return
+	}
+
+	data := struct {
+		Amount      decimal.Decimal `json:"amount"`
+		Description string          `json:"description"`
+		PaidBy      string          `json:"paid_by"`
+	}{}
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		log.Printf("Couldn't decode request body: %v\n", err)
+		http.Error(w, "Malformed request", http.StatusBadRequest)
+		return
+	}
+
+	if data.Amount.LessThan(decimal.Zero) {
+		data.Amount = expense.Amount
+	}
+
+	if data.Description == "" {
+		data.Description = expense.Description
+	}
+
+	paidByID := expense.PaidBy
+	if data.PaidBy != "" {
+		paidBy, err := cfg.Db.GetUserByUsername(r.Context(), data.PaidBy)
+		if err != nil {
+			log.Printf("Couldn't find user: %v\n", err)
+			http.Error(w, fmt.Sprintf("Couldn't find user %s", data.PaidBy), http.StatusBadRequest)
+			return
+		}
+
+		paidByID.Valid = true
+		paidByID.UUID = paidBy.ID
+	}
+
+
+	expense, err = cfg.Db.UpdateExpense(
+		r.Context(),
+		database.UpdateExpenseParams{
+			ID:          expense.ID,
+			PaidBy:      paidByID,
+			Description: data.Description,
+			Amount:      data.Amount,
+		},
+	)
+	if err != nil {
+		log.Printf("Couldn't update expense: %v\n", err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	usersInGroup, err := cfg.Db.GetUsersByGroup(r.Context(), tx.GroupID)
+	if err != nil {
+		log.Printf("Couldn't get users in group: %v\n", err)
+		http.Error(w, "Couldn't find group", http.StatusBadRequest)
+		return
+	}
+
+	debtAmount := expense.Amount.Div(decimal.NewFromInt(int64(len(usersInGroup))))
+
+	if err := cfg.Db.DeleteDebtsByExpense(r.Context(), expense.ID); err != nil {
+		log.Printf("Couldn't delete debts for expense: %v\n", err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	for _, u := range usersInGroup {
+		if expense.PaidBy.Valid && expense.PaidBy.UUID == u.ID {
+			continue
+		}
+
+		if _, err := cfg.Db.CreateDebt(
+			r.Context(),
+			database.CreateDebtParams{
+				ExpenseID: expense.ID,
+				OwedTo: expense.PaidBy,
+				OwedBy: uuid.NullUUID{Valid: true, UUID: u.ID},
+				Amount: debtAmount,
+			},
+		); err != nil {
+			log.Printf("Couldn't create debt: %v\n", err)
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if _, err := cfg.Db.UpdateTransaction(r.Context(), tx.ID); err != nil {
+		log.Printf("Couldn't update transaction: %v\n", err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
