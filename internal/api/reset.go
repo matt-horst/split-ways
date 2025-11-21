c68ded5efc8d4f5bfe48d86089f2b939863c1d8c
Matt Horst <mattahorst@gmail.com>
2025-11-20 19:54:46 -0700

    removes newlines from error strings

--- internal/api/reset.go
+++ internal/api/reset.go
@@ -11,22 +11,22 @@ import (
 func Reset(ctx context.Context, db *sql.DB, queries *database.Queries) error {
 	tx, err := db.Begin()
 	if err != nil {
-		return fmt.Errorf("couldn't begin transaction: %v\n", err)
+		return fmt.Errorf("couldn't begin transaction: %v", err)
 	}
 	defer tx.Rollback()
 
 	qtx := queries.WithTx(tx)
 
 	if err := qtx.DeleteAllGroups(ctx); err != nil {
-		return fmt.Errorf("couldn't delete all groups: %v\n", err)
+		return fmt.Errorf("couldn't delete all groups: %v", err)
 	}
 
 	if err := qtx.DeleteAllUsers(ctx); err != nil {
-		return fmt.Errorf("couldn't delete all users: %v\n", err)
+		return fmt.Errorf("couldn't delete all users: %v", err)
 	}
 
 	if err := tx.Commit(); err != nil {
-		return fmt.Errorf("couldn't commit transaction: %v\n", err)
+		return fmt.Errorf("couldn't commit transaction: %v", err)
 	}
 
 	return nil
