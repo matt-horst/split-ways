d8fd873203a9b3a17f65461b4b66d07ab3a2e066
Matt Horst <mattahorst@gmail.com>
2025-11-20 19:35:19 -0700

    adds integration test for user creation

--- go.mod
+++ go.mod
@@ -3,16 +3,23 @@ module github.com/matt-horst/split-ways
 go 1.25.3
 
 require (
-	github.com/a-h/templ v0.3.960 // indirect
-	github.com/alexedwards/argon2id v1.0.0 // indirect
-	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/a-h/templ v0.3.960
+	github.com/alexedwards/argon2id v1.0.0
+	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/sessions v1.4.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+	github.com/shopspring/decimal v1.4.0
+	github.com/stretchr/testify v1.11.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
-	github.com/gorilla/sessions v1.4.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/shopspring/decimal v1.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ tests/users_test.go
@@ -0,0 +1,88 @@
+package tests
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/matt-horst/split-ways/handlers"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"database/sql"
+
+	"github.com/gorilla/sessions"
+
+	"github.com/matt-horst/split-ways/internal/api"
+	"github.com/matt-horst/split-ways/internal/database"
+
+	_ "github.com/lib/pq"
+)
+
+var cfg *handlers.Config
+
+func TestMain(m *testing.M) {
+	db, err := sql.Open("postgres", "postgresql://postgres:postgres@localhost:5432/split_ways_test?sslmode=disable")
+	if err != nil {
+		log.Fatalf("could not connect to database: %v\n", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("could not ping database: %v\n", err)
+	}
+
+	queries := database.New(db)
+
+	cfg = &handlers.Config{
+		DB:     db,
+		Queries: queries,
+		Store:  sessions.NewCookieStore([]byte("session_key")),
+		JwtKey: "jwt_key",
+	}
+
+	exitCode := m.Run()
+
+	// TODO: Reset database in teardown
+	if err := api.Reset(context.Background(), db, queries); err != nil {
+		log.Fatalf("could not clean up test database: %v\n", err)
+	}
+
+	os.Exit(exitCode)
+}
+
+func TestCreateUser(t *testing.T) {
+	// Test successful creation of user
+	type createUserData struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	data := createUserData{
+		Username: "user",
+		Password: "password",
+	}
+
+	body, err := json.Marshal(data)
+	require.NoError(t, err)
+
+	r := httptest.NewRequest("POST", "/api/users", bytes.NewBuffer(body))
+	rr := httptest.NewRecorder()
+
+	cfg.HandlerCreateUser(rr, r)
+
+	assert.Equal(t, rr.Code, http.StatusNoContent)
+
+	// Test creating user with taken username responds with error
+	r = httptest.NewRequest("POST", "/api/users", bytes.NewBuffer(body))
+	rr = httptest.NewRecorder()
+
+	cfg.HandlerCreateUser(rr, r)
+
+	assert.Equal(t, rr.Code, http.StatusBadRequest)
+}
